db5f3574409a82960cd38dc2d47a427dd0f16a03
Cuong Nguyen Van <cuong.nv281203@gmail.com>
2025-03-26 15:02:10 +0700

    add dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
-// import router from './router'
+import router from './router' // Ensure this matches your folder structure
 import './assets/main.css'
 
 const app = createApp(App)
 app.use(createPinia())
-// app.use(router)
+app.use(router) // Router must be registered here
 app.mount('#app')
